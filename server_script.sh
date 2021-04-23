c88d10cd774de0f6e2c184729efa152afa5e9d4a
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-04-24 01:19:21 +0300

    Update server_script.sh

--- server_script.sh
+++ server_script.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# Filesystem and mounting
 sudo yum -y install parted
 sudo parted -s /dev/sdb mklabel gpt
 sudo parted /dev/sdb mkpart primary ext4 0% 100%
@@ -7,3 +8,13 @@ sudo mkfs.ext4 /dev/sdb1
 mkdir /var/backup
 sudo mount /dev/sdb1 /var/backup
 cat /etc/mtab | grep /dev/sdb >> /etc/fstab
+
+# Borg install and adjust
+sudo yum -y install epel-release
+sudo yum -y install borgbackup
+sudo useradd -m borg
+mkdir /home/borg/.ssh
+sudo chmod 0700 /home/borg/.ssh/
+cp /vagrant/authorized_keys /home/borg/.ssh
+sudo chown borg:borg -R /home/borg
+sudo chmod 0600 /home/borg/.ssh/authorized_keys
