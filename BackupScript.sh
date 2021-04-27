e76026f67e557edeba95fa34484bf52d6a330a67
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-04-28 01:34:58 +0300

    Update BackupScript.sh

--- BackupScript.sh
+++ BackupScript.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#Переменные окружения Borg
+#Задаем переменную окружения Borg
 export BORG_PASSPHRASE="borg"
 REPOSITORY="borg@borgserver:/var/backup/borg"
 
@@ -8,10 +8,10 @@ REPOSITORY="borg@borgserver:/var/backup/borg"
 echo "Copy files to server"
 borg create -v --stats $REPOSITORY::'Backup-{now:%Y-%m-%d@%H:%M}' /etc
 
-# Удаление лишних бэкапов. Глубина бекапа год, храним по последней копии на конец месяца, кроме последних трех. Последние три месяца содержат копии на каждый день.
+# По ТЗ - глубина бекапа год, храним по последней копии на конец месяца, кроме последних трех. Последние три месяца содержат копии на каждый день.
 echo "Clean old files" 
 borg prune -v $REPOSITORY --prefix 'Backup-' --list --keep-daily=90 --keep-monthly=9 --keep-yearly 1
 
-#Добавим вывод информации о репозитории
+#Вывод информации о содержимом репозитория
 echo "Backups list"
 borg list $REPOSITORY
