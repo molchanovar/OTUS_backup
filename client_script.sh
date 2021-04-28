85ce61deae7084edcbedf84ef1d60e249c643fff
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-04-28 20:23:41 +0300

    Update client_script.sh

--- client_script.sh
+++ client_script.sh
@@ -5,11 +5,11 @@ sudo yum -y install borgbackup
 sudo yum -y install vim
 sudo useradd -m borg
 cp /vagrant/hosts /etc/
-cp /vagrant/id_rsa /home/borg/.ssh/
-cp /vagrant/id_rsa.pub /home/borg/.ssh/
-sudo mkdir /var/backup
-sudo chmod 700 /var/backup
-sudo chown borg:borg /var/backup
+sudo cp /vagrant/id_rsa /home/borg/.ssh/
+sudo cp /vagrant/id_rsa.pub /home/borg/.ssh/
+# sudo mkdir /var/backup
+# sudo chmod 700 /var/backup
+# sudo chown borg:borg /var/backup
 borg init --encryption=repokey borg@server:/var/backup/borg --stdin borg
 cp /vagrant/BackupScript.sh /opt
 sudo chmod +x /opt/BackupScript.sh
